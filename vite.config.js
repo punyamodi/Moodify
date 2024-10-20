f0fefd86ae2f75843d12d4fc7e6ca32c71d273c9
Punya Modi <modipunya@gmail.com>
2024-10-21 02:15:19 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Moodify/', // This should be correct for your deployment
+  base: '/Moodify/', // Only if your app is served from /Moodify/
 });
